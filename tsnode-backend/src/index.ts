1cec50c763c559e520e55810f9b8f500efcc1acf
Dan Seagren <seagrendaniel562@gmail.com>
2025-03-14 08:50:26 -0700

    add find best theater query to Node API and update script in package.json to spin up API with 'npm start'

--- tsnode-backend/src/index.ts
+++ tsnode-backend/src/index.ts
@@ -1,32 +1,63 @@
 import * as dotenv from 'dotenv'
-dotenv.config({path: '../.env'})
-
-import express from 'express'
+import express, {Request, Response} from 'express'
 import {Pool} from 'pg'
 
-const app = express()
-const port = 3001
-const pgPassword = process.env.PG_PASSWORD;
+dotenv.config({path: '../.env'})
 
+
+const app = express()
+const port = process.env.PORT || 4000
+
+
+// Create PSQL Pool
 const pool = new Pool({
-  user: 'admin',
-  host: 'localhost',
-  database: 'movie_theater_analytics',
-  password: pgPassword,
-  port: 5432,
+  user: process.env.DB_USER || 'admin',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'movie_theater_analytics',
+  password: process.env.PG_PASSWORD,
+  port: Number(process.env.DB_PORT) || 5432
 })
 
+app.get('/api/best_theater', async (req: Request, res: Response): Promise<void> => {
+  const sale_date: string = req.query.sale_date as string
 
-app.get('/', async (req, res) => {
-  try{
-    const result = await pool.query('SELECT NOW()')
-    res.json({message: 'Hello from Node API', dbTime: result.rows[0]})
+  // Confirm sale_date param exists in API request
+  if (!sale_date) {
+    res.status(400).json({error: 'Please provide a sale_date parameter in the format YYYY-MM-DD.'})
+    return
+  }
+
+  // Validate sale_date format
+  try {
+    new Date(sale_date)
   } catch (err) {
-    if (err instanceof Error) {
-      res.status(500).json({error: err.message})
+    res.status(400).json({error: 'Invalid sale_date provided. Please use the format YYYY-MM-DD.'})
+    return
+  }
+
+
+  const FIXED_TICKET_PRICE = 10.00;
+  const queryBestTheater = `
+    SELECT t.id AS theater_id, t.company AS company_name, t.location,
+           COUNT(s.id) AS total_sales, COUNT(s.id) * $2 AS total_revenue
+    FROM tickets s
+    JOIN theater t ON s.theaterId = t.id
+    WHERE s.sale_date = $1
+    GROUP BY t.id, t.company, t.location
+    ORDER BY total_sales DESC
+    LIMIT 1;
+  `;
+
+  try {
+    const result = await pool.query(queryBestTheater, [sale_date, FIXED_TICKET_PRICE])
+    if (result.rows.length > 0) {
+      res.json(result.rows[0])
     } else {
-      res.status(500).json({error: 'An unexpected error occured'})
+      res.status(404).json({message: 'No data found for the given date.'})
     }
+  } catch (err) {
+    console.error('Error executing query', err)
+    res.status(500).json({error: 'Internal server errror.'})
   }
 })
 
